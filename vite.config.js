1788eb733d6283f964f40e2486264270df5fc7a5
AbhishekPujari <147771502+Abhishekworld7@users.noreply.github.com>
2025-07-28 11:40:36 +0530

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-reusable-components/", // 🔥 This is important
+  base: "/react-reusable-components/", // 👈 Very important!
 })
